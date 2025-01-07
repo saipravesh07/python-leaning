0b22acd5c2a313051b89d691f0035a87ffb6dd02
saipravesh07 <saipravesh.j07@gmail.com>
2025-01-07 10:16:33 +0530

    cal

--- cal.py
+++ cal.py
@@ -1,2 +1,11 @@
-if 5>2:
-  print("hello")
+num1 = 10
+num2 = 20
+#addition two number
+add = num1+num2
+sub = num2-num1
+mul = num1*num2
+div = num1/num2
+print(add)
+print(sub)
+print(mul)
+print(div)
